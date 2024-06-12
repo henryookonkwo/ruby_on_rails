9b1d59f280fa07e27c70053ad01334ed76e3727a
Henry Okonkwo <47309196+henryookonkwo@users.noreply.github.com>
2024-06-12 09:56:05 -0400

    fun with the controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
   end
 
   def about
+    @about_me = "Hi I'm Henry Okonkwo, a Fullstack Developer learning Ruby on Rails!!!!"
+    @answer = 2 + 2
   end
 end
